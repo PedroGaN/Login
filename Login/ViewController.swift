c0136ba294ef65ca6cfa0476dbc6c3174e9bc821
PedroGaN <pgarcianavalon@gmail.com>
2020-10-02 16:02:04 +0200

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Apps2t on 02/10/2020.
+//  Copyright © 2020 PedroGaN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
