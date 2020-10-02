a4c5c7cf6c3383010b666183d09330f0e31d2ff6
PedroGaN <pgarcianavalon@gmail.com>
2020-10-02 17:40:01 +0200

    Connection between views

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    var userText: String? = "No username found"
+    var passText: String? = "No password found"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func OnSelectedUser(_ sender: UITextField){
+        userText = sender.text
+    }
+    
+    @IBAction func OnSelectedPassword(_ sender: UITextField){
+        passText = sender.text
+    }
+    
+    @IBAction func OnLoginButtonPressed(_ sender: UIButton){
+        print(userText!,passText!)
+    }
 }
 
---
+++ Login/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  Login
+//
+//  Created by Apps2t on 02/10/2020.
+//  Copyright © 2020 PedroGaN. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+}
+
